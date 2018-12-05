0bb3d7afe33bb789089351194602c041df658414
Maker <lcwjyouyi@163.com>
2018-12-05 18:11:38 +0800

    树莓派开发平台

--- routes/control.js
+++ routes/control.js
@@ -7,14 +7,20 @@ router.get('/', function(req, res, next) {
     res.render('control');
 });
 
+//语音
 router.post('/ajax/talk', function(req, res, next){
-    console.log(req.body.say);
     var command = 'python3 pi-command/synthesis.py "'  + req.body.say +'"';
     child_process.exec(command, function(err, stdout, stderr){
 		if(err) throw err;
         res.send(stderr||"1");
     });
 });
+
+//设备
+router.post('/ajax/device', function(req, res, next){
+    console.log(req.body);
+});
+
 
 
 module.exports = router;
